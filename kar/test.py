b932af30b4d135f3e6173a343ca73e86a8cface9
Sergey Mikayelyan <serimikayelyan@gmail.com>
2021-04-03 13:48:48 +0400

    Added tik/ and kar/ folders

---
+++ kar/test.py
@@ -0,0 +1,5 @@
+import time
+print("helloKAr")
+for i in range(5):
+    print (i)
+    time.sleep(0.5)
---
+++ tik/test.py
@@ -0,0 +1,5 @@
+import time
+print("helloTik")
+for i in range(5):
+    print (i)
+    time.sleep(0.5)
